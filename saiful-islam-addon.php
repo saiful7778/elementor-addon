936ddc415c5da47ee4f998ace4091fc1a8793362
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-22 19:06:41 +0600

    add header requirements

---
+++ saiful-islam-addon.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* Plugin Name: Saiful Islam Addon
+* Description: This is custom elementor addon made by Saiful Islam.
+* Plugin URI: https://example.com
+* Version: 1.0.0
+* Require At Least: 5.2
+* Requires PHP: 7.2
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Author: Saiful Islam
+* Author URI: https://portfolio-saiful7778.vercel.app
+* Text Domain: saisad
+* Requires Plugins: elementor
+* Elementor tested up to: 3.20.1
+* Elementor Pro tested up to: 3.20.0
+*/
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
